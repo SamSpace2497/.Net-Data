49f35f6b7747720b99fa8b144e440216cb6fc94d
Shubham Patil <118250165+SamSpace2497@users.noreply.github.com>
2023-01-14 12:13:17 +0530

    Add files via upload

---
+++ Day5/Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] arr1 = new int[] { 1, 2, 3 };
+            int[] arr2 = new int[3] { 1, 2, 3};
+            int[] arr = new int[5];
+            //int[] arr  --- arr is an object of the (integer array) class
+            //(integer array) class inherits from the System.Array class
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //Console.WriteLine("Enter element no " + i.ToString());  //string concatenation
+                //Console.WriteLine("Enter element no {0} ", i);  //placeholder
+                Console.Write($"Enter element no {i} ");  //string interpolation
+
+                arr[i] = int.Parse(Console.ReadLine());
+                //arr[i] = Convert.ToInt32 (Console.ReadLine());
+            }
+            Console.WriteLine();
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Day5/NullableTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace NullableTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? i;
+            i = 100;
+            i = null;
+
+            int j;
+            if (i != null)
+                j = (int)i;
+            else
+                j = 0;
+
+            if (i.HasValue)
+                j = i.Value;
+            else
+                j = 0;
+
+            j = i.GetValueOrDefault();
+            j = i.GetValueOrDefault(10);
+            j = i ?? 0; //null coalescing operator
+
+            Console.WriteLine(j);
+        }
+    }
+
+    public class Employee
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set {  name = value; }
+        }
+    }
+}
